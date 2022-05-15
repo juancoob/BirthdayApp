5549cb9bf1649bb5a93eef307f57213ea99a8865
juancoob <ja.cobos.obrero@gmail.com>
2022-05-15 10:31:10 +0200

    feat: Create user list layout

--- app/src/main/java/com/example/birthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdayapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.birthdayapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.birthdayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.userList.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
     }
 }
